69c8fba60ed2fe0da34f02caf794db8348166624
Stephen Pardue <spardue@github.com>
2018-11-14 19:41:18 -0500

    added comments, and cleaned up code

--- problem1.scala
+++ problem1.scala
@@ -7,21 +7,26 @@ case class Rate(rateCode: String, rateGroup: String)
 case class CabinPrice(cabinCode: String, rateCode: String, price: BigDecimal)
 case class BestGroupPrice(cabinCode: String, rateCode: String, price: BigDecimal, rateGroup: String)
 
+/*
+Get best group prices by cabin
+*/
 def getBestGroupPrices(rates: Seq[Rate], prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
     
     val cabins = prices.map(_.cabinCode).distinct
 
-
     val rateGroup = rates.foldLeft(Map[String,String]()) { (acc, rate) =>
         acc + (rate.rateCode -> rate.rateGroup) 
-    }
+    } // Map the rate codes to their rateGroup. This O(1) vs O(n) for the given data structure  
+
 
     cabins.flatMap { cabin =>
 
-        val cabinPrices = prices.filter(_.cabinCode == cabin)
+        val cabinPrices = prices.filter(_.cabinCode == cabin) //only filter on prices for the cabin
 
-        rates.map ( rate => {
-            val min = cabinPrices.filter(price => rateGroup(price.rateCode) == rateGroup(rate.rateCode))
+        // For each rate, find the lowest price for a cabin
+        rates.map ( rate => { 
+            // We only care about the lowest price for a rate group, hence the rateGroup lookup
+            val min = cabinPrices.filter(price => rateGroup(price.rateCode) == rateGroup(rate.rateCode)) 
             .minBy(_.price)
             BestGroupPrice(min.cabinCode, min.rateCode, min.price, rate.rateGroup)  
         }).distinct
--- problem2.scala
+++ problem2.scala
@@ -8,6 +8,18 @@ case class PromotionCombo(promotionCodes: Seq[String])
 
 
 
+
+/*
+Generates a map of promotions to combinable promptions.
+Example output: 
+Map(
+    P3 -> List(P2, P4, P5), 
+    P5 -> List(P1, P3, P4), 
+    P2 -> List(P1, P3), 
+    P1 -> List(P2, P4, P5), 
+    P4 -> List(P1, P3, P5))
+This is useful because the input relationship is in the other direction. Promotions to non combinable promotions.
+*/
 def getComboMap(allPromotions: Seq[Promotion]): Map[String, Seq[String]] = {
     allPromotions.foldLeft(Map[String, Seq[String]]()) { (acc, p) => 
         val combinableCodes = allPromotions.filter { op => op != p && ! op.notCombinableWith.contains(p.code)}.map(_.code)
@@ -15,43 +27,42 @@ def getComboMap(allPromotions: Seq[Promotion]): Map[String, Seq[String]] = {
     }
 }
 
+/* 
+Returns the longest combinable promotions for a promotion. 
+*/
 def combinablePromotions(promotionCode: String, allPromotions: Seq[Promotion]): Seq[PromotionCombo] =  {
 
-    val comboMap = getComboMap(allPromotions)
+    val comboMap = getComboMap(allPromotions) 
 
-    val codes = comboMap.keys
-    
+    val seed = promotionCode :: comboMap(promotionCode).toList //Seed combination is the longest possible combination 
 
-    val seed = promotionCode :: comboMap(promotionCode).toList
-
-    val combos = (2 to seed.length).flatMap(seed.combinations(_))
+    val combos = (2 to seed.length).flatMap(seed.combinations(_)) // Generate all the possible sub combinations from the seed combination
     
 
     val validCombos = combos.filter { combo => 
         ! combo.flatMap { promotion =>
             combo.filter(_ != promotion).map ( oPromotion => comboMap(promotion).contains(oPromotion) )
-        }.contains(false)
+        }.contains(false) //validate the combos by making sure every promotion in a combo list is compatiable 
     }
 
     val subCombos = validCombos.flatMap { combo => 
         (2 to combo.length).flatMap(combo.combinations(_)).filter(_ != combo).toList
-    }
+    } //generate all the subcombinations of the valid combinations
 
-    val longestCombos = validCombos.filter { combo => ! subCombos.contains(combo) }
+    val longestCombos = validCombos.filter { combo => ! subCombos.contains(combo) } //filter out the valid combinations that are subcombinations
 
     longestCombos.map(combo => PromotionCombo(combo.sorted))
 
-
-
 }
 
+/* 
+Get the longest combinable promotions for each promotion
+*/
 def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] =  {
 
-    getComboMap(allPromotions)
-    .keys
-    .flatMap( promo => combinablePromotions(promo, allPromotions))
-    .toSeq
-
+    val promotionCodes = allPromotions.map(_.code).toSet
+    
+    promotionCodes.flatMap( promo => combinablePromotions(promo, allPromotions)).toSeq
 
 }
 
